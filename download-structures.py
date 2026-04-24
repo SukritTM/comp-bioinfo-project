f9b1d6d2c11a2a04f782d5c7dc5fc255749ff387
SukritTM <sukrittmani@gmail.com>
2026-04-24 16:41:29 -0400

    changes

--- download-structures.py
+++ download-structures.py
@@ -4,6 +4,8 @@ import Bio.PDB.alphafold_db as adb
 from datasets import load_dataset
 from tqdm import tqdm
 
+from time import perf_counter as pf
+
 ds = load_dataset("tattabio/euk_retrieval")
 
 train = ds['train'].to_pandas()
@@ -11,10 +13,15 @@ test = ds['test'].to_pandas()
 
 num_multiple = 0
 no_results = []
-for i, uniprot_id in enumerate(train['Entry']):
+results = []
+
+timer = pf()
+print('Training subset')
+for i, uniprot_id in enumerate(tqdm(train['Entry'])):
     # print(uniprot_id, flush=True)
-    try: 
-        pred = list(adb.get(uniprot_id))
+    try:
+        pred = list(adb.get_predictions(uniprot_id))
+        results.append(pred)
     except:
         pred = []
         no_results.append(uniprot_id)
@@ -22,5 +29,25 @@ for i, uniprot_id in enumerate(train['Entry']):
     if len(pred) > 1:
         num_multiple += 1
 
-print(f'num_multiple: {num_multiple}')
+for i, uniprot_id in enumerate(tqdm(test['Entry'])):
+    # print(uniprot_id, flush=True)
+    try:
+        pred = list(adb.get_predictions(uniprot_id))
+        results.append(pred)
+    except:
+        pred = []
+        no_results.append(uniprot_id)
+    # print(i, len(pred))
+    if len(pred) > 1:
+        num_multiple += 1
+
+
+timer = pf() - timer
+
+
 print(f'no_results: {no_results}')
+
+print(f'time: {timer:2f}')
+print(f'num_multiple: {num_multiple}')
+print(f'num with no results: {len(no_results)}')
+print(f'num with results: {len(results)}')
